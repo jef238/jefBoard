af1e02dbde4b055f32bc78c39ae58c103d6f593a
jef238 <122973929+jef238@users.noreply.github.com>
2024-02-26 19:57:07 +0100

    ...

---
+++ esp8266/script2.lua
@@ -0,0 +1,10 @@
+if wifi.sta.getip()== nil then
+    print("IP unavailable, Waiting...")
+  else
+    tmr.stop(1)
+    print("\n====================================")
+    print("ESP8266 mode is: " .. wifi.getmode())
+    print("MAC address is: " .. wifi.ap.getmac())
+    print("IP is "..wifi.sta.getip())
+    print("====================================")    
+  end
